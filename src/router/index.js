53a4df58ec34c5e7280e2eac73b34845b44a71b7
seungyong88 <gosng2001@naver.com>
2021-07-12 14:00:50 +0900

    라우터 이름 추가

--- src/router/index.js
+++ src/router/index.js
@@ -14,29 +14,34 @@ export const router = new VueRouter({
   routes: [
     {
       path:'/',
+      name: 'home',
       redirect: '/news',
-      
     },
     {
       // path : url 주소
       path: '/news',
+      name: 'news',
       // component: url 주소로 갔을 때 표시될 컴포넌트
       component: NewsView,
     },
     {
       path: '/jobs',
+      name: 'jobs',
       component: JobsView,
     },
     {
       path: '/ask',
+      name: 'ask',
       component: AskView,
     },
     {
       path: '/user/:id',
+      name: 'user',
       component: UserView,
     },
     {
       path: '/item/:id',
+      name: 'item',
       component: ItemView,
     }
   ]
