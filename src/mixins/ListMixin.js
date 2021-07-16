e369d1c271bad2ed9c8ff2d2ba539e9feab23b76
seungyong88 <gosng2001@naver.com>
2021-07-16 15:47:58 +0900

    믹스인 추가

--- src/mixins/ListMixin.js
+++ src/mixins/ListMixin.js
@@ -4,14 +4,12 @@ export default {
   // 재사용할 컴포넌트 옵션
   created() {
     bus.$emit('start:spinner');
-    setTimeout(() => {
-      this.$store.dispatch('FETCH_LIST', this.$route.name)
-        .then((res) => {
-          bus.$emit('end:spinner');
-        })
-        .catch(err => console.log(err));
-
-    }, 200)
+    
+    this.$store.dispatch('FETCH_LIST', this.$route.name)
+      .then((res) => {
+        bus.$emit('end:spinner');
+      })
+      .catch(err => console.log(err));
   }
 
 }
